1ab816b68646b2ed454396f9050ecb89de41feee
XY-ZHOU <731986251@qq.com>
2019-02-14 23:14:30 +0800

    instantiate printer

--- src/model/Printer.java
+++ src/model/Printer.java
@@ -4,5 +4,11 @@ public class Printer {
     public void print() {
         System.out.println("打印文件");
     }
-    
+
+    public static void main(String[] args) {
+        Printer colorPrinter = new ColorPrinter();
+        colorPrinter.print();
+        Printer notColorPrinter = new NotColorPrinter();
+        notColorPrinter.print();
+    }
 }
